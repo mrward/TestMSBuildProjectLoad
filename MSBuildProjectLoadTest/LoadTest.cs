c3d68de56944b6a6ea4205c5b6c3073768f7f38f
Matt Ward <matt.ward@xamarin.com>
2017-01-04 15:15:01 +0000

    Add test console project that loads .NET Core sdk project

---
+++ MSBuildProjectLoadTest/LoadTest.cs
@@ -0,0 +1,85 @@
+﻿//
+// LoadTest.cs
+//
+// Author:
+//       Matt Ward <matt.ward@xamarin.com>
+//
+// Copyright (c) 2017 Xamarin Inc. (http://xamarin.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.IO;
+using System.Xml;
+using Microsoft.Build.Construction;
+using Microsoft.Build.Evaluation;
+
+namespace MSBuildProjectLoadTest
+{
+	static class LoadTest
+	{
+		static string file = @"TestDotNetCore.csproj";
+
+		public static void Run ()
+		{
+			var paths = new DotNetCoreSdkPaths ();
+			paths.FindSdkPaths ("Microsoft.NET.Sdk");
+
+			//LoadProjectFromFile ();
+			LoadProjectFromXml2 ();
+		}
+
+		static void LoadProjectFromFile ()
+		{
+			var engine = new ProjectCollection { DefaultToolsVersion = "15.0" };
+			Project p = engine.LoadProject (file);
+			Console.WriteLine (p.FullPath);
+		}
+
+		/// <summary>
+		/// Throws an Microsoft.Build.Exceptions.InvalidProjectFileException:
+		/// The value \"\" of the \"Project\" attribute in element <Import> is invalid.
+		/// Parameter \"path\" cannot have zero length.  /usr/local/share/dotnet/sdk/1.0.0-preview5-004275/Sdks/Microsoft.NET.Sdk/Sdk/Sdk.targets\n 
+		/// 
+		/// The problem is that the .NET Core Sdk imports need the MSBuildProjectExtension property
+		/// defined. This is only defined if the FullPath is set on the Project. This cannot be
+		/// set when ProjectCollection.LoadProject is passed an XmlTextReader.
+		/// </summary>
+		static void LoadProjectFromXml ()
+		{
+			var engine = new ProjectCollection { DefaultToolsVersion = "15.0" };
+			string content = File.ReadAllText (file);
+			Project p = engine.LoadProject (new XmlTextReader (new StringReader (content)));
+			p.FullPath = file;
+		}
+
+		static void LoadProjectFromXml2 ()
+		{
+			var engine = new ProjectCollection { DefaultToolsVersion = "15.0" };
+			string content = File.ReadAllText (file);
+
+			var projectRoot = ProjectRootElement.Create (new XmlTextReader (new StringReader (content)));
+			projectRoot.FullPath = file;
+			string toolsVersion = projectRoot.ToolsVersion ?? engine.DefaultToolsVersion;
+			var p = new Project (projectRoot, null, toolsVersion, engine);
+
+			Console.WriteLine (p.FullPath);
+		}
+	}
+}
---
+++ MSBuildProjectLoadTest/Program.cs
@@ -0,0 +1,72 @@
+﻿//
+// Program.cs
+//
+// Author:
+//       Matt Ward <matt.ward@xamarin.com>
+//
+// Copyright (c) 2017 Xamarin Inc. (http://xamarin.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace MSBuildProjectLoadTest
+{
+	class MainClass
+	{
+		static string msbuildBinDir = @"/Library/Frameworks/Mono.framework/Versions/4.8.0/lib/mono/msbuild/15.0/bin/";
+		//static string msbuildBinDir = @"~/projects/utils/msbuild/bin/Bootstrap";
+		[STAThread]
+		public static void Main (string[] args)
+		{
+			try {
+				AppDomain.CurrentDomain.AssemblyResolve += MSBuildAssemblyResolver;
+
+				LoadTest.Run ();
+			} catch (Exception ex) {
+				Console.WriteLine (ex);
+			}
+		}
+
+		static Assembly MSBuildAssemblyResolver (object sender, ResolveEventArgs args)
+		{
+			var msbuildAssemblies = new string[] {
+				"Microsoft.Build",
+				"Microsoft.Build.Engine",
+				"Microsoft.Build.Framework",
+				"Microsoft.Build.Tasks.Core",
+				"Microsoft.Build.Utilities.Core" };
+
+			var asmName = new AssemblyName (args.Name);
+			if (!msbuildAssemblies.Any (n => string.Compare (n, asmName.Name, StringComparison.OrdinalIgnoreCase) == 0))
+				return null;
+
+			string fullPath = Path.Combine (msbuildBinDir, asmName.Name + ".dll");
+			if (File.Exists (fullPath)) {
+				// If the file exists under the msbuild bin dir, then we need
+				// to load it only from there. If that fails, then let that exception
+				// escape
+				return Assembly.LoadFrom (fullPath);
+			} else
+				return null;
+		}
+	}
+}
